5ed2684c843c27dede60ac1fa4796bfd75c111ec
amukoaterry <amukoaterrymitchell@gmail>
2024-08-29 13:58:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loaderContainers = document.querySelectorAll('.loader');
+  const pageContent = document.querySelector('#page-content');
+
+});
